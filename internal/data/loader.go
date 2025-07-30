ab41ff1408eed11a11e77db4ae85293faf91f64b
Johnny Huang <hzqtc1229@gmail.com>
2025-07-29 17:28:19 -0700

    Try loading data from either ledger and hledger.

--- internal/data/loader.go
+++ internal/data/loader.go
@@ -1,29 +1,41 @@
 package data
 
 import (
+	"errors"
+	"os"
 	"os/exec"
 )
 
 func LoadTransactions() ([]Transaction, error) {
-	cmd := exec.Command("hledger", "print")
+	commands := []string{"ledger", "hledger"}
 
-	// Get a pipe to the command's standard output
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		panic(err)
+	for _, cmd := range commands {
+		cmd := exec.Command(cmd, "print")
+
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			return nil, err
+		}
+
+		if err := cmd.Start(); err != nil {
+			if errors.Is(err, exec.ErrNotFound) {
+				continue
+			} else {
+				return nil, err
+			}
+		}
+
+		// Stream output to parser
+		transactions, parseErr := parseJournal(stdout)
+		// Wait for command to complete
+		if err := cmd.Wait(); err != nil {
+			return nil, err
+		} else if parseErr != nil {
+			return nil, parseErr
+		} else {
+			return transactions, nil
+		}
 	}
 
-	// Start the command
-	if err := cmd.Start(); err != nil {
-		panic(err)
-	}
-
-	transactions, parseErr := parseJournal(stdout)
-	if err := cmd.Wait(); err != nil {
-		return nil, err
-	} else if parseErr != nil {
-		return nil, parseErr
-	} else {
-		return transactions, nil
-	}
+	return nil, os.ErrNotExist
 }
