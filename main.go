705b761ac53b001aaa559908a7527d64b80e9943
Johnny Huang <hzqtc1229@gmail.com>
2025-08-07 22:04:42 -0700

    Add debug flag to disable panic catching and show stack traces.

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 )
 
 var flagShowHelp = pflag.BoolP("help", "h", false, "Show help message")
+var flagDebug = pflag.Bool("debug", false, "Enable debug mode to print stack trace on panic")
 
 func main() {
 	pflag.Parse()
@@ -28,7 +29,11 @@ func main() {
 	// Send log output to the file
 	log.SetOutput(f)
 
-	p := tea.NewProgram(model.NewModel(), tea.WithAltScreen())
+	opts := []tea.ProgramOption{tea.WithAltScreen()}
+	if *flagDebug {
+		opts = append(opts, tea.WithoutCatchPanics())
+	}
+	p := tea.NewProgram(model.NewModel(), opts...)
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
